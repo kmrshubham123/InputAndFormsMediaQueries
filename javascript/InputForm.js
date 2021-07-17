5478220c48511f08fadd0f40d2aa1f42296afa61
kmrshubham123 <kmrshubham123@gmail.com>
2021-07-17 21:21:08 +0530

    [Kumar Shubham]Added:Enter a Valid Email

--- javascript/InputForm.js
+++ javascript/InputForm.js
@@ -8,3 +8,13 @@ text.addEventListener('input', function () {
     else
         textError.textContent = "Name is incorrect";
 });
+//UC2:-User need to Enter valid Email
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.email-error');
+email.addEventListener('input', function () {
+    let pattern = RegExp('^[A-Z a-z]{1,}([+-_.]*)[A-Z a-z 0-9 _+-.]*[@]{1}[A-Z a-z 0-9 +_-]{1,}[.]{1}[a-z]{2,3}([.]{1}[a-z]{2})*$');
+    if (pattern.test(email.value))
+        emailError.textContent = '';
+    else
+        emailError.textContent = "Email is Incorrect";
+});
