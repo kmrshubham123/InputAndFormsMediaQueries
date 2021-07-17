4eb4d4c3442a5c20b34e5dbe0171b1a12424f492
kmrshubham123 <kmrshubham123@gmail.com>
2021-07-17 21:31:47 +0530

    [Kumar Shubham]Added:Pre-defined Mobile Format

--- javascript/InputForm.js
+++ javascript/InputForm.js
@@ -18,3 +18,16 @@ email.addEventListener('input', function () {
     else
         emailError.textContent = "Email is Incorrect";
 });
+//UC3:-User Need to follow Pre-defined Mobile format
+const tel = document.querySelector('#tel');
+const telError = document.querySelector('.tel-error');
+tel.addEventListener('input', function () 
+{
+    let telRegex = RegExp('^[0-9]{2}[ ][0-9]{10}$');
+    //Checking number is valid or not using if else condition
+    if (telRegex.test(tel.value)) 
+        telError.textContent = "";
+    else 
+        telError.textContent = "telephone number is not Valid";
+    
+});
